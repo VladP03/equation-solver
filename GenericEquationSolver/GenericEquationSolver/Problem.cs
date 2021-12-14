56ca5beefbac0af6ac305cce0250f69a4ef737bd
xeno-john <ionut.alexandru.baltariu@gmail.com>
2021-12-14 23:08:49 +0200

    Add initial application skeleton +gitignore

---
+++ GenericEquationSolver/GenericEquationSolver/Problem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEquationSolver
+{
+    class Problem : IOptimizationProblem
+    {
+        Func<double> equationFunction;
+        public Problem(string equationString)
+        {
+            // trebuie sa gandim o metoda de a parsa o functie de grad n dintr-un string
+        }
+
+        public void ComputeFitness(Chromosome c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chromosome MakeChromosome()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenericEquationSolver/GenericEquationSolver/Solve.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEquationSolver
+{
+    public class Selection
+    {
+        private static Random _rand = new Random();
+
+        public static Chromosome Tournament(Chromosome[] population)
+        {
+            throw new Exception("Aceasta metoda trebuie implementata");
+        }
+
+        public static Chromosome GetBest(Chromosome[] population)
+        {
+            throw new Exception("Aceasta metoda trebuie implementata");
+        }
+    }
+
+    //==================================================================================
+
+    /// <summary>
+    /// Clasa care reprezinta operatia de incrucisare
+    /// </summary>
+    public class Crossover
+    {
+        private static Random _rand = new Random();
+
+        public static Chromosome Arithmetic(Chromosome mother, Chromosome father, double rate)
+        {
+            throw new Exception("Aceasta metoda trebuie implementata");
+        }
+    }
+
+    //==================================================================================
+
+    /// <summary>
+    /// Clasa care reprezinta operatia de mutatie
+    /// </summary>
+    public class Mutation
+    {
+        private static Random _rand = new Random();
+
+        public static void Reset(Chromosome child, double rate)
+        {
+            throw new Exception("Aceasta metoda trebuie implementata");
+        }
+    }
+
+    //==================================================================================
+
+    /// <summary>
+    /// Clasa care implementeaza algoritmul evolutiv pentru optimizare
+    /// </summary>
+    public class DifferentialEvolutionaryAlgorithm
+    {
+        /// <summary>
+        /// Metoda de optimizare care gaseste solutia problemei
+        /// </summary>
+        public Chromosome Solve(IOptimizationProblem p, int populationSize, int maxGenerations, double crossoverRate, double motivationRate)
+        {
+            throw new Exception("Aceasta metoda trebuie completata");
+        }
+    }
+}
