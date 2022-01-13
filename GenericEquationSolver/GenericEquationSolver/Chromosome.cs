d6636f1c6e2c18233ea8c3fab4fad86d1b25376a
Pavăl Mihaela-Irina <67002201+pmihaela-irina@users.noreply.github.com>
2022-01-13 09:36:25 +0200

    Update Chromosome.cs

--- GenericEquationSolver/GenericEquationSolver/Chromosome.cs
+++ GenericEquationSolver/GenericEquationSolver/Chromosome.cs
@@ -1,3 +1,19 @@
+/**************************************************************************
+ *                                                                        *
+ *  Copyright:   (c) 2016-2020, Florin Leon                               *
+ *  E-mail:      florin.leon@academic.tuiasi.ro                           *
+ *  Website:     http://florinleon.byethost24.com/lab_ia.html             *
+ *  Description: Evolutionary Algorithms                                  *
+ *               (Artificial Intelligence lab 8)                          *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
 using System;
 
 namespace GenericEquationSolver
